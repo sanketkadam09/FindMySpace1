ddebd20171596088b971f8a86bca8e6f8fcd6f53
Sanket Kadam <sk8185710@gmail.com>
2025-12-04 23:56:28 +0530

    Fixed App.js for Render deployment

--- server/App.js
+++ server/App.js
@@ -94,12 +94,12 @@ io.on("connection", (socket) => {
 });
 
 // ----------------- DEPLOYMENT: Serve React Build -----------------
-const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "client/build")));
+  // Correct path: server → ../client/build
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
